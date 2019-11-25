c199557bd44c0e6946f0fc16b2c2b75deebfad93
dani <mayor@dani.town>
2019-11-24 22:09:34 -0500

    undo

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e76b8fac2af695e84145";
+/******/ 	var hotCurrentHash = "6734e3cc8c27728f3b82";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
